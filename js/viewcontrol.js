e5709345a415a6f16655108b58abf282a46b98a2
Nikunj Gupta <nikunj9180@gmail.com>
2024-05-09 23:02:17 +0530

    initial commit

--- js/viewcontrol.js
+++ js/viewcontrol.js
@@ -4,12 +4,17 @@ const b1=document.getElementById('btn1');
 const b2=document.getElementById('btn2');
 
 cont1.classList.add("active");
+b1.classList.add("active");
 
 b1.addEventListener('click',()=>{
     cont1.classList.add("active")
+    b1.classList.add("active");
+    b2.classList.remove("active");
     cont2.classList.remove("active");
 });
 b2.addEventListener('click',()=>{
     cont2.classList.add("active")
+    b2.classList.add("active");
+    b1.classList.remove("active");
     cont1.classList.remove("active");
 });
